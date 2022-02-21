d786507364b9e36fedf2e6a55da78ef9a4805d39
Thái Quý <thaivanquy2503@gmail.com>
2022-02-21 08:47:19 +0700

    change JS_20-02-2022.txt to JS_20-02-200.JS

---
+++ JS_20-02-2022.js
@@ -0,0 +1,34 @@
+
+	//Bien va khai bao bien
+		var fullName; // khai bao bien
+		fullName = 'Thai Quy'; // Gan gia tri cho bien
+	//hoac viet ngan gon
+		var fullName = 'Thai Quy';
+
+/* 
+	Ham built-in 
+	1. Alert //Bat len cua so hop thoai
+	2. Confirm //Bat len cua so hop thoai co them 2 nut Ok va Cancel
+	3. Prompt //Tuung tu confirm nhung co them o de nhap.
+	4. Console
+		- console.log // hien thi ra loi nhan tren tab console cua chorme
+		- console.warn // hien thi ra loi nhan canh bao
+		- console.error //hien thi ra loi nhan loi 
+	5. setTimeout //Thuc thi 1 ham code sau 1 thoi gian cho truoc nhung chi thuc thi 1 lan 
+	6. setInterval //Tuong tu nhu setTimeout nhung lap lai nhieu lan
+*/
+
+	//1. Toan tu gan 
+		var fullName = 'Thai Quy';	
+	//2. Toan tu so sanh (>, <, ==)	
+		var a = 2;
+		var b = 1;
+		if (a > b) {
+    		alert('Dung');
+		}
+    //3. Toan tu logic 
+		var a = 1;
+		var b = 3;
+		if (a > 0 && b > 0 ) {
+    		alert('a & b deu lon hon 0');
+		}
